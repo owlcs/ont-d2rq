4d34c9755273dae3cda88b45bb7b89d3ff50e701
szuev <sergei.zuev@avicomp.ru>
2018-11-06 18:23:08 +0300

    ont-d2rq: add control over CachingGraph size, add test (issue #7)

--- src/main/java/de/fuberlin/wiwiss/d2rq/jena/CachingGraph.java
+++ src/main/java/de/fuberlin/wiwiss/d2rq/jena/CachingGraph.java
@@ -3,39 +3,105 @@ package de.fuberlin.wiwiss.d2rq.jena;
 import org.apache.jena.atlas.lib.Cache;
 import org.apache.jena.atlas.lib.CacheFactory;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.graph.impl.GraphBase;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.apache.jena.util.iterator.WrappedIterator;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
 /**
- * A GraphD2RQ that caches the results of the most recently performed queries on an LRU basis.
+ * A {@code CachingGraph} that caches the results of the most recently performed queries on an LRU basis.
  * Notice that it is a read only accessor.
- * TODO: implement caching iterator
+ *
  * @author Holger Knublauch (holger@topquadrant.com)
  * Created by @ssz on 27.10.2018.
  */
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class CachingGraph extends GraphBase {
+
+    protected static final List<Triple> TOO_BIG = new ArrayList<>();
 
     protected final Cache<Triple, List<Triple>> triples;
     protected final Graph base;
+    protected final int maxCacheSize;
+    protected final long maxLength;
+    protected final int bucketCapacity;
+    protected long count;
 
+    /**
+     * Creates a caching graph with default settings.
+     * Here the length limit is taken equal to {@code 30_000_000} that roughly matches 60mb
+     * (grossly believing that a java(8) String consists only of chars).
+     *
+     * @param base {@link Graph} to wrap, not {@code null}
+     */
     public CachingGraph(Graph base) {
-        this(Objects.requireNonNull(base), 10_000);
+        this(Objects.requireNonNull(base), 10_000, 30_000_000);
     }
 
-    public CachingGraph(Graph base, int maxSize) {
-        this.base = Objects.requireNonNull(base, "Null graph");
-        triples = CacheFactory.createCache(maxSize);
+    /**
+     * Creates a caching graph, that keeps track of its own size.
+     *
+     * @param base      {@link Graph} to wrap, not {@code null}
+     * @param cacheSize int, the cache size
+     * @param maxLength long, max number of chars that this cache can hold
+     */
+    public CachingGraph(Graph base, int cacheSize, long maxLength) {
+        this(base, cacheSize, maxLength, maxLength > cacheSize ? (int) (maxLength / cacheSize) : cacheSize);
+    }
+
+    /**
+     * Creates a caching graph, that keeps track of its own size.
+     * If the queried bunch size is too large to fit in the cache, then an uncached iterator is returned.
+     *
+     * @param base           {@link Graph} to wrap, not {@code null}
+     * @param cacheSize      int, the cache size
+     * @param maxLength      long, max number of chars that this cache can hold
+     * @param bucketCapacity int, the default bucket capacity
+     * @see LengthTripleIterator
+     */
+    public CachingGraph(Graph base, int cacheSize, long maxLength, int bucketCapacity) {
+        this.base = Objects.requireNonNull(base, "Null graph.");
+        this.maxCacheSize = requireNonNegative(cacheSize, "Negative cache size");
+        this.maxLength = requireNonNegative(maxLength, "Negative max length");
+        this.bucketCapacity = requireNonNegative(bucketCapacity, "Negative default bucket size");
+        this.triples = CacheFactory.createCache(cacheSize);
+    }
+
+    private static <N extends Number> N requireNonNegative(N n, String msg) {
+        if (n.intValue() < 0) {
+            throw new IllegalArgumentException(msg);
+        }
+        return n;
     }
 
     @Override
     public ExtendedIterator<Triple> graphBaseFind(Triple m) {
-        return WrappedIterator.create(triples.getOrFill(m, () -> base.find(m).toList()).iterator());
+        List<Triple> res = triples.getIfPresent(m);
+        if (TOO_BIG == res) {
+            return base.find(m);
+        } else if (res != null) {
+            return WrappedIterator.create(res.iterator());
+        }
+        // do caching:
+        LengthTripleIterator bucket = new LengthTripleIterator(base.find(m));
+        res = new ArrayList<>(bucketCapacity);
+        while (bucket.hasNext()) res.add(bucket.next());
+        ((ArrayList<Triple>) res).trimToSize();
+        long current = bucket.getLength();
+        if (count + current < maxLength) {
+            triples.put(m, res);
+            count += current;
+        } else {
+            // not enough space in the cache
+            triples.put(m, TOO_BIG);
+        }
+        return WrappedIterator.create(res.iterator());
     }
 
     /**
@@ -44,5 +110,59 @@ public class CachingGraph extends GraphBase {
      */
     public void clearCache() {
         triples.clear();
+    }
+
+    /**
+     * A {@link WrappedIterator} that calculates its length while iterating,
+     * assuming that "length" of a {@link Triple} is equal (or proportional)
+     * the number of characters in its string representation.
+     * Language tag for literal nodes is not taken into account.
+     * This iterator doesn't permit removing.
+     */
+    public static class LengthTripleIterator extends WrappedIterator<Triple> {
+        private final double uriNodeFactor;
+        private final double literalNodeFactor;
+        private final double blankNodeFactor;
+        private long counter;
+
+        public LengthTripleIterator(Iterator<Triple> base) {
+            this(base, 1, 1, 1);
+        }
+
+        public LengthTripleIterator(Iterator<Triple> base,
+                                    double uriFactor,
+                                    double bNodeFactor,
+                                    double literalFactor) {
+            super(base, true);
+            uriNodeFactor = requireNonNegative(uriFactor, "Negative uri node factor");
+            blankNodeFactor = requireNonNegative(bNodeFactor, "Negative blank node factor");
+            literalNodeFactor = requireNonNegative(literalFactor, "Negative literal node factor");
+        }
+
+        @Override
+        public Triple next() {
+            Triple res = super.next();
+            counter += calc(res);
+            return res;
+        }
+
+        protected long calc(Triple t) {
+            return calc(t.getSubject()) + calc(t.getPredicate()) + calc(t.getObject());
+        }
+
+        protected long calc(Node n) {
+            if (n.isURI()) return calc(n.getURI(), uriNodeFactor);
+            if (n.isLiteral()) return calc(n.getLiteral().getLexicalForm(), literalNodeFactor);
+            if (n.isBlank()) return calc(n.getBlankNodeLabel(), blankNodeFactor);
+            throw new IllegalStateException("Can't calculate node length: " + n);
+        }
+
+        public static long calc(String txt, double factor) {
+            return (long) (txt.length() * factor);
+        }
+
+        public long getLength() {
+            return counter;
+        }
     }
 }
--- src/test/java/de/fuberlin/wiwiss/d2rq/jena/CachingGraphTest.java
+++ src/test/java/de/fuberlin/wiwiss/d2rq/jena/CachingGraphTest.java
@@ -1,5 +1,7 @@
 package de.fuberlin.wiwiss.d2rq.jena;
 
+import org.apache.jena.atlas.iterator.Iter;
+import org.apache.jena.atlas.lib.Cache;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.mem.GraphMem;
@@ -23,10 +25,9 @@ import tmp.Tmp_Caching;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
 
 /**
  * Created by @szz on 06.11.2018.
@@ -75,10 +76,44 @@ public class CachingGraphTest {
             LOGGER.debug("Expected: '{}'", dde.getMessage());
         }
 
-
+        Cache<Triple, List<Triple>> cache = ((CachingGraph) m.getBaseGraph()).triples;
+        Set<Triple> keys = Iter.asStream(cache.keys()).collect(Collectors.toSet());
+        for (Triple t : keys) {
+            if (map.containsKey(t)) continue;
+            Assert.fail("Not in map: " + t);
+        }
+        for (Triple t : map.keySet()) {
+            if (keys.contains(t)) continue;
+            Assert.fail("Not in cache: " + t);
+        }
         map.entrySet().stream()
                 .sorted(Comparator.comparingLong(o -> o.getValue().longValue()))
                 .forEach(e -> LOGGER.debug("{}:::{}", e.getValue().longValue(), e.getKey()));
         map.forEach((t, l) -> Assert.assertEquals("Incorrect count for " + t, 1, l.longValue()));
+    }
+
+    @Test
+    public void testSmallCache() {
+        String uri = "http://x";
+        OntGraphModel m = OntModelFactory.createModel()
+                .setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", uri + "#");
+        m.setID(uri);
+        OntClass c = m.createOntEntity(OntClass.class, uri + "#Class");
+        c.addLabel("TheClass");
+        OntIndividual i = c.createIndividual(uri + "#Individual");
+        i.addComment("This is individual");
+
+        m = OntModelFactory.createModel(new CachingGraph(m.getBaseGraph(), 10, 3));
+        m.write(SINK, "ttl");
+        Assert.assertEquals(1, m.ontObjects(OntCE.class).peek(x -> LOGGER.debug("{}", x)).count());
+        Assert.assertEquals(1, m.ontObjects(OntIndividual.class).peek(x -> LOGGER.debug("{}", x)).count());
+        Assert.assertEquals(6, m.size());
+        Cache<Triple, List<Triple>> cache = ((CachingGraph) m.getBaseGraph()).triples;
+        Assert.assertEquals(10, cache.size());
+        cache.keys().forEachRemaining(x -> Assert.assertTrue(cache.getOrFill(x, () -> {
+            throw new AssertionError("No value");
+        }).isEmpty()));
+        Assert.assertSame(CachingGraph.TOO_BIG, cache.getIfPresent(i.getRoot().asTriple()));
+        Assert.assertSame(CachingGraph.TOO_BIG, cache.getIfPresent(c.getRoot().asTriple()));
     }
 }
